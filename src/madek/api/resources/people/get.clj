69dc99c65e76644aa68f2c915240add00ba7532b
Thomas Schank <DrTom@schank.ch>
2023-12-19 10:19:00 +0100

    WIP people #psh

--- src/madek/api/resources/people/get.clj
+++ src/madek/api/resources/people/get.clj
@@ -5,7 +5,6 @@
    [logbug.debug :as debug]
    [madek.api.authorization :as authorization]
    [madek.api.db.core :refer [get-ds]]
-   [madek.api.resources.people.common :refer [wrap-find-person]]
    [madek.api.resources.shared :as sd]
    [madek.api.utils.auth :refer [wrap-authorize-admin!]]
    [madek.api.utils.logging :as logging]
--- src/madek/api/resources/people/main.clj
+++ src/madek/api/resources/people/main.clj
@@ -4,7 +4,7 @@
     ;[madek.api.resources.people.create :as create-person]
     ;[madek.api.resources.people.delete :as delete-person]
    [madek.api.resources.people.get :as get-person]
-   [madek.api.resources.people.index :as index]
+   ;[madek.api.resources.people.index :as index]
     ;[madek.api.resources.people.update :as update-person]
    [madek.api.utils.logging :as logging]
    [taoensso.timbre :refer [debug error info spy warn]]))
@@ -12,7 +12,7 @@
 (def routes
   ["/people"
    ["/"
-    {:get index/route
+    {;:get index/route
      ;:post create-person/route
      }]
    ["/:id"
