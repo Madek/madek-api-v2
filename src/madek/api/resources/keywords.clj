1047bffe13dea5b3dc81c7454df0f3d9a663e202
mradl <manuel.radl@zhdk.ch>
2024-06-11 15:38:58 +0200

    keywords v1

--- src/madek/api/resources/keywords.clj
+++ src/madek/api/resources/keywords.clj
@@ -12,6 +12,7 @@
     [madek.api.utils.helper :refer [str-to-int]]
     [next.jdbc :as jdbc]
     [reitit.coercion.schema]
+    [madek.api.utils.pagination :refer [pagination-handler swagger-ui-pagination]]
     [schema.core :as s]
     [clojure.spec.alpha :as sa]))
 
@@ -178,299 +179,8 @@
           (sd/response_failed (str "Invalid query parameters: " errors) 400))))))
 
 
-;(defn move-params [schema]
-;
-;  (fn [handler]
-;    (fn [request]
-;      (let [;           params (-> request :parameters :query)
-;
-;      ;; process casting of query parameters
-;
-;             cast            (fn [m]
-;                               (reduce-kv
-;                                (fn [acc k v]
-;                                  (assoc acc k
-;                                         (cond
-;                                           (= k :page) (str-to-int v v)
-;                                           (= k :size) (str-to-int v v)
-;                                           :else       v)))
-;                                {}
-;                                m))
-;             request         (assoc-in request [:params] (cast (-> request :params)))
-;             params          (-> request :params)
-;
-;
-;             _               (let [;                  p (println ">o> request=" request)
-;
-;
-;        ;                  params (-> request :parameters :query)
-;        ;                            params          (-> request :params)
-;                                    [valid? errors] (s/validate schema params)
-;                                    p               (println ">o> validation-result: " valid? errors)]
-;                               (if-not valid?
-;                                 ;               (handler request)
-;                                 ;               (sd/response_bad_request (str "Invalid query parameters: " errors) 400)))
-;                                 (sd/response_bad_request (str "Invalid query parameters: " errors) errors)))
-;
-;
-;             params2         (-> request :params :query)
-;             qparams         (-> request :query)
-;
-;
-;             p               (println ">o> 1?params=" request);
-;             p               (println ">o> 1params=" params)
-;             p               (println ">o> 2params2=" params2)
-;             p               (println ">o> 3qparams=" qparams)
-;
-;             ;; swagger-ui
-;             p               (println ">o> 3qparams=" (-> request :params)) ;{:page 2, :size 3}
-;             p               (println ">o> 3qparams=" (-> request :query-params)); {:page 2, :size 3}
-;
-;
-;             ; move (-> request :params) to (-> request :parameters :query)
-;
-;             ;           request (assoc request :parameters (assoc (-> request :parameters) :query (-> request :params)))
-;
-;
-;             key-map         {:size :count}
-;             rename          (fn [m key-map]
-;                               (reduce-kv
-;                                (fn [acc k v]
-;                                  (let [new-key (get key-map k k)] ; Get the new key from key-map or use the original key if not found
-;                                    (assoc acc new-key v)))
-;                                {}
-;                                m))
-;
-;             p               (println ">o> 1cast.before" (-> request :params))
-;             p               (println ">o> 2cast.after" (cast (-> request :params)))
-;             p               (println ">o> 3cast.after" (rename (cast (-> request :params)) key-map))
-;
-;             ;           request (assoc-in request [:parameters :query] (cast (-> request :params)))
-;
-;             ;           request (assoc request :parameters (-> request :parameters :query))
-;
-;             p               (println ">o> before=" (-> request :parameters :query))
-;
-;             ;           request (assoc-in request [:parameters :query] params)
-;             ;             request (assoc-in request [:parameters :query] (rename (cast (-> request :params)) key-map))
-;             request         (assoc-in request [:parameters :query] (rename (-> request :params) key-map))
-;             p               (println ">o> >>  1after=" (-> request :parameters :query))
-;             p               (println ">o> 2after=" (-> request :parameters :query :page))
-;             p               (println ">o> 3after=" (class (-> request :parameters :query :page)))
-;
-;
-;
-;             ;          page (or (get params :page) 0)
-;             ;          size (or (get params :size) 10)
-;             ;          new-params (assoc params :page page :size size)
-;             ]
-;        ;      (handler parameters :query new-params))))))
-;        (handler request)))))
-
-
-;(defn move-params [schema]
-;  (fn [handler]
-;    (fn [request]
-;
-;      (try
-;
-;
-;        (let [; Function to cast parameters
-;               cast            (fn [m]
-;                                 (reduce-kv
-;                                  (fn [acc k v]
-;                                    (assoc acc k
-;                                           (cond
-;                                             (= k :page) (str-to-int v v)
-;                                             (= k :size) (str-to-int v v)
-;                                             :else       v)))
-;                                  {}
-;                                  m))
-;
-;               ; Function to rename keys in a map based on key-map
-;               rename          (fn [m key-map]
-;                                 (reduce-kv
-;                                  (fn [acc k v]
-;                                    (let [new-key (get key-map k k)] ; Get the new key from key-map or use the original key if not found
-;                                      (assoc acc new-key v)))
-;                                  {}
-;                                  m))
-;
-;               ; Cast the parameters and update the request map
-;               casted-vals     (cast (-> request :params))
-;               request         (assoc-in request [:params] casted-vals)
-;               request         (assoc-in request [:parameters :query] casted-vals)
-;
-;               params          (-> request :params)
-;
-;               ; Validate the schema
-;               [valid? errors] (s/validate schema params)]
-;
-;          ; Print debug information
-;          (println ">o> validation-result: " valid? errors)
-;          (println ">o> 1?params=" request)
-;          (println ">o> 1params=" params)
-;
-;          ;        (if-not valid?
-;          ;          (do
-;          ;            (println ">o> Validation failed")
-;          ;            (sd/response_bad_request (str "Invalid query parameters: " errors) errors))
-;          (do
-;            ; Rename the keys in params based on key-map
-;            (let [key-map        {:size :count}
-;                  params-renamed (rename params key-map)]
-;
-;              ; Update the request with the renamed parameters
-;              (let [request (assoc-in request [:parameters :query] params-renamed)]
-;                (println ">o> 1cast.before" (-> request :params))
-;                (println ">o> 2cast.after" (cast (-> request :params)))
-;                (println ">o> 3cast.after" params-renamed)
-;                (println ">o> before=" (-> request :parameters :query))
-;                (println ">o> >>  1after=" (-> request :parameters :query))
-;                (println ">o> 2after=" (-> request :parameters :query :page))
-;                (println ">o> 3after=" (class (-> request :parameters :query :page)))
-;
-;                ; Call the handler with the updated request
-;                (handler request))))
-;          ;        )
-;          )
-;
-;
-;        (catch Exception ex
-;
-;          (println ">o> abc" ex)
-;          (sd/response_bad_request ex
-;                                   (sd/response_bad_request (str "Invalid query parameters: " (.getMessage ex)) ex)))))))
-
-;(s/def ::positive-number (and number? pos?))
-
-
-;; Adding metadata to the spec
-;(defn with-doc [spec doc-string]
-;  (sa/with-gen spec
-;              (fn []
-;                (sa/gen spec))
-;              (assoc (meta spec) :doc doc-string)))
-
-;;; Adding metadata to the spec
-;(defn with-doc [spec doc-string]
-;  (with-meta spec {:doc doc-string}))
-
-
-;(def ^:const page (with-meta ::positive-number {:doc "Page must be a positive number including zero"}))
-
-
-;(sa/def ::positive-number (sa/and ::positive-number-def (with-meta {:doc "Size must be a positive number including zero"})))
-;(sa/def schema {:size (with-doc ::positive-number "my posNumber") :page (with-doc ::positive-number "mypos2")})
-
-;(sa/def schema {:size  ::positive-number  :page  (with-meta ::positive-number {:doc "Page must be a positive number including zero"}) })
-
-
 (sa/def ::positive-number (sa/and number? #(>= % 0)))
 (sa/def schema {:size ::positive-number :page ::positive-number})
-
-
-;;; Define a positive number including zero
-;(sa/def ::positive-number
-;       (sa/and number? #(>= % 0)))
-;
-;;; Helper function to add metadata to specs
-;(defn with-doc [spec doc-string]
-;  (let [spec (sa/spec spec)]
-;    (vary-meta spec assoc :doc doc-string)))
-;
-;;; Define specs with documentation
-;(sa/def ::size (with-doc ::positive-number "Size must be a positive number including zero"))
-;(sa/def ::page (with-doc ::positive-number "Page must be a positive number including zero"))
-;
-;;; Define the schema
-;(sa/def ::schema
-;       (sa/keys :req-un [::size ::page]))
-
-
-;;; Define a positive number including zero / broken
-;(sa/def ::positive-number
-;       (sa/and number? #(>= % 0)))
-;
-;;; Attach metadata directly to the spec definitions
-;(def ^:doc "Size must be a positive number including zero"
-;  ::size
-;  (sa/and ::positive-number))
-;
-;(def ^:doc "Page must be a positive number including zero"
-;  ::page
-;  (sa/and ::positive-number))
-;
-;;; Define the schema
-;(sa/def ::schema
-;       (sa/keys :req-un [::size ::page]))
-
-
-;;; Define a positive number including zero
-;(s/def ::positive-number
-;       (s/and number? #(>= % 0)))
-;
-;;; Define the specs with metadata
-;(defn positive-number-spec [doc]
-;  (let [spec (s/and ::positive-number)]
-;    (with-meta spec {:doc doc})))
-;
-;(s/def ::size (positive-number-spec "Size must be a positive number including zero"))
-;(s/def ::page (positive-number-spec "Page must be a positive number including zero"))
-;
-;;; Define the schema
-;(s/def ::schema
-;       (s/keys :req-un [::size ::page]))
-
-
-(defn pagination-handler
-  "Required query-fields: page & size
-
-  Workflow:
-    - rename :page to :count (needed for internal handling).
-    - cast :page & :size to int.
-    - validate :page & :size (exception returns response with status-code=400 and validation-details).
-  "
-  [schema]
-  (fn [handler]
-    (fn [request]
-      (try
-        (let [cast-fnc        (fn [m]
-                                (println ">o> [cast]" m)
-                                (reduce-kv
-                                 (fn [acc k v]
-                                   (assoc acc k
-                                          (cond
-                                           (= k :page) (str-to-int v v)
-                                           (= k :size) (str-to-int v v)
-                                           :else       v)))
-                                 {}
-                                 m))
-
-              rename-keys-fnc (fn [m key-map]
-                                (println ">o> [rename]" m key-map)
-                                (reduce-kv
-                                 (fn [acc k v]
-                                   (let [new-key (get key-map k k)]
-                                     (assoc acc new-key v)))
-                                 {}
-                                 m))
-
-              casted-vals     (cast-fnc (-> request :params))
-              request         (let [casted-vals (cast-fnc (-> request :params))]
-                                (let [request (assoc-in request [:params] casted-vals)]
-                                  (assoc-in request [:parameters :query] casted-vals)))
-              params          (-> request :params)
-              _               (s/validate schema params)]
-
-          (let [key-map {:size :count}
-                request (let [params-renamed (rename-keys-fnc params key-map)]
-                          (let [request (assoc-in request [:params] params-renamed)]
-                            (assoc-in request [:parameters :query] params-renamed)))]
-
-            (handler request)))
-        (catch Exception ex
-          (sd/response_bad_request (str "Invalid query parameters: " (.getMessage ex)) (.getData ex)))))))
 
 
 (defn wrap-find-keyword [handler]
@@ -493,23 +203,20 @@
      {:summary    (sd/sum_pub (d "Query / list keywords."))
       :handler    handle_usr-query-keywords
       :coercion   reitit.coercion.schema/coercion
-
-      ;      :parameters {:query ItemQueryParams}
-
-      ;      :middleware [(validate-scheme ItemQueryParams)]
-      ;      :middleware [move-params ] ;;ok
       :middleware [(pagination-handler ItemQueryParams)]
 
-      :swagger    {:parameters [{:name        "page"
-                                 :in          "query"
-                                 :description "Page number, defaults to 0"
-                                 :required    false
-                                 :value       "1"}
-                                {:name        "size"
-                                 :in          "query"
-                                 :description "Number of items per page, defaults to 10"
-                                 :required    false
-                                 :value       "11"}]}
+      :swagger (swagger-ui-pagination)
+
+;      :swagger    {:parameters [{:name        "page"
+;                                 :in          "query"
+;                                 :description "Page number, defaults to 0"
+;                                 :required    false
+;                                 :value       "1"}
+;                                {:name        "size"
+;                                 :in          "query"
+;                                 :description "Number of items per page, defaults to 10"
+;                                 :required    false
+;                                 :value       "11"}]}
 
 
       :responses  {200 {:body {:keywords [schema_export_keyword_usr]}}
@@ -540,20 +247,21 @@
     {:get
      {:summary     (sd/sum_adm "Query keywords")
       :handler     handle_adm-query-keywords
-      :middleware  [;                     wrap-authorize-admin!
-                     (pagination-handler ItemQueryParams)]
+      :middleware  [                     wrap-authorize-admin!
+                     (pagination-handler (merge schema_query_keyword ItemQueryParams))]
 
-      :swagger     {:parameters [{:name        "page"
-                                  :in          "query"
-                                  :description "Page number, defaults to 0 (zero-based)"
-                                  :required    true
-                                  :value       "1"}
-                                 {:name        "size"
-                                  :in          "query"
-                                  :description "Number of items per page, defaults to 10"
-                                  :required    true
-                                  :value       "11"}]}
+;      :swagger     {:parameters [{:name        "page"
+;                                  :in          "query"
+;                                  :description "Page number, defaults to 0 (zero-based)"
+;                                  :required    true
+;                                  :value       "1"}
+;                                 {:name        "size"
+;                                  :in          "query"
+;                                  :description "Number of items per page, defaults to 10"
+;                                  :required    true
+;                                  :value       "11"}]}
 
+      :swagger (swagger-ui-pagination)
       :coercion    reitit.coercion.schema/coercion
       :parameters  {:query schema_query_keyword}
       :responses   {200 {:body {:keywords [schema_export_keyword_adm]}}}
--- src/madek/api/resources/keywords/keyword.clj
+++ src/madek/api/resources/keywords/keyword.clj
@@ -10,18 +10,7 @@
   (dbh/query-eq-find-one :keywords :id id tx))
 
 (defn db-keywords-query [query tx]
-  (let [
-         p (println ">o> query"  (->
-                                (sql/select :*)
-                                (sql/from :keywords)
-                                (dbh/build-query-param query :id)
-                                (dbh/build-query-param query :rdf_class)
-                                (dbh/build-query-param-like query :meta_key_id)
-                                (dbh/build-query-param-like query :term)
-                                (dbh/build-query-param-like query :description)
-                                (pagination/add-offset-for-honeysql query)
-                                sql-format))
-         dbq (->
+  (let [dbq (->
              (sql/select :*)
              (sql/from :keywords)
              (dbh/build-query-param query :id)
