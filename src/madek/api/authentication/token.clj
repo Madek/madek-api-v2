b399f7810671719b15475aa838533629c2542605
mradl <manuel.radl@zhdk.ch>
2024-08-06 10:30:08 +0200

    revert but with modifications

--- src/madek/api/authentication/token.clj
+++ src/madek/api/authentication/token.clj
@@ -47,35 +47,70 @@
     (#{:delete :put :post :patch}
      (:request-method request))))
 
-(defn authenticate [user-token is-admin? handler request]
-;(defn authenticate [user-token handler request]
-  (println ">o> user-token???=" user-token)
-  (println ">o> is-admin????=" is-admin?)
-  (println ">o> uri????=" (:uri request))
-  (cond
-    (:token_revoked user-token) {:status 401
-                                 :body "The token has been revoked."}
 
-    ; TODO: reactivate it!!
-    (violates-not-read?
-      user-token request) {:status 403
-                           :body (str "The token is not allowed to read"
-                                      " i.e. to use safe http verbs.")}
-    (violates-not-write?
-      user-token request) {:status 403
-                           :body (str "The token is not allowed to write"
-                                      " i.e. to use unsafe http verbs.")}
+(defn pr [str fnc]
+  (println ">oo> HELPER / " str)
+  fnc
+  )
 
-    (and (str/includes? (:uri request) "/api-v2/admin/") (not is-admin?) ) {:status 401
-                                 :body "The token has no admin-privileges."}
+;(defn authenticate [user-token is-admin? handler request]
+;  ;(defn authenticate [user-token handler request]
+;  (println ">o> user-token???=" user-token)
+;  (println ">o> is-admin????=" is-admin?)
+;  (println ">o> uri????=" (:uri request))
+;  (cond
+;    (:token_revoked user-token) {:status 401
+;                                 :body "The token has been revoked."}
+;
+;    ; TODO: reactivate it!!
+;    (violates-not-read?
+;      user-token request) {:status 403
+;                           :body (str "The token is not allowed to read"
+;                                      " i.e. to use safe http verbs.")}
+;    (violates-not-write?
+;      user-token request) {:status 403
+;                           :body (str "The token is not allowed to write"
+;                                      " i.e. to use unsafe http verbs.")}
+;
+;    (and (str/includes? (:uri request) "/api-v2/admin/") (not is-admin?)) {:status 401
+;                                                                           :body "The token has no admin-privileges."}
+;
+;    :else (do
+;            (println ">o> set admin now to _> " (sd/is-admin (:user_id user-token) (:tx request)))
+;            (handler
+;              (pr ">o> updated-request: " (assoc request
+;                                                 :authenticated-entity (assoc user-token :type "User")
+;                                                 ; TODO move into ae
+;                                                 :is_admin (sd/is-admin (:user_id user-token) (:tx request)))))))) ;; TODO: db: admins
 
-    :else (do
-            (println ">o> set admin now to _> " (sd/is-admin (:user_id user-token) (:tx request)))
-            (handler
+(defn authenticate [user-token handler request]
+
+  (let [
+        is-admin? (sd/is-admin (:user_id user-token) (:tx request)) ;; admins
+        ]
+
+    (cond
+      (:token_revoked user-token) {:status 401
+                                   :body "The token has been revoked."}
+      (violates-not-read?
+        user-token request) {:status 403
+                             :body (str "The token is not allowed to read"
+                                        " i.e. to use safe http verbs.")}
+      (violates-not-write?
+        user-token request) {:status 403
+                             :body (str "The token is not allowed to write"
+                                        " i.e. to use unsafe http verbs.")}
+
+
+      (and (str/includes? (:uri request) "/api-v2/admin/") (not is-admin?)) {:status 401
+                                                                             :body "The token has no admin-privileges."}
+
+      :else (handler
               (assoc request
                      :authenticated-entity (assoc user-token :type "User")
                      ; TODO move into ae
-                     :is_admin (sd/is-admin (:user_id user-token) (:tx request))))))) ;; TODO: db: admins
+                     :is_admin is-admin?))))                ;; TODO: db: admins
+  )
 
 (defn find-token-secret-in-header [request]
   (when-let [header-value (-> request :headers keywordize-keys :authorization)]
@@ -83,83 +118,93 @@
       (last (re-find #"(?i)^token\s+(.+)$" header-value)))))
 
 
-(defn- get-auth-systems-user [userId tx]
-  (jdbc/execute-one! tx (-> (sql/select :*)
-                            (sql/from :auth_systems_users)  ;; TODO:  db: auth_systems_users
-                            (sql/where [:= :user_id userId] [:= :auth_system_id "password"])
-                            sql-format)))
+;(defn- get-auth-systems-user [userId tx]
+;  (jdbc/execute-one! tx (-> (sql/select :*)
+;                            (sql/from :auth_systems_users)  ;; TODO:  db: auth_systems_users
+;                            (sql/where [:= :user_id userId] [:= :auth_system_id "password"])
+;                            sql-format)))
+;
+;
+;(defn- get-user-by-login-or-email-address [login-or-email tx]
+;  (->> (jdbc/execute! tx (-> (sql/select :*)
+;                             (sql/from :users)
+;                             (sql/where [:or [:= :login login-or-email] [:= :email login-or-email]])
+;                             sql-format))
+;    (map #(assoc % :type "User"))
+;    (map #(clojure.set/rename-keys % {:email :email_address}))
+;    first))
 
 
-(defn- get-user-by-login-or-email-address [login-or-email tx]
-  (->> (jdbc/execute! tx (-> (sql/select :*)
-                             (sql/from :users)
-                             (sql/where [:or [:= :login login-or-email] [:= :email login-or-email]])
-                             sql-format))
-    (map #(assoc % :type "User"))
-    (map #(clojure.set/rename-keys % {:email :email_address}))
-    first))
+;(defn find-and-authenticate-token-secret-or-continue [handler request]
+;  (println ">o> abc1")
+;  (if-let [token-secret (find-token-secret-in-header request)]
+;
+;    (let [
+;          _ (println ">o> abc1, TS=" token-secret)
+;          user-token (find-user-token-by-some-secret [token-secret] (:tx request))
+;          p (println ">o> user-token=" user-token)
+;
+;
+;          user_id (:user_id user-token)
+;          email (:email_address user-token)
+;
+;          ;(authenticate user-token handler request))
+;
+;          entity (get-user-by-login-or-email-address email (:tx request))
+;
+;          p (println ">o> user_id=" user_id)
+;          p (println ">o> email=" email)
+;
+;          is-admin-auth-sys-user? (get-auth-systems-user user_id (:tx request))
+;          p (println ">o> 1 is-admin-auth-sys-user?=" is-admin-auth-sys-user?)
+;
+;          is-admin-admins? (sd/is-admin user_id (:tx request)) ;;admins
+;          p (println ">o> 2 is-admin-admins?=" is-admin-admins?)
+;
+;
+;          request (assoc request
+;                         :authenticated-entity entity
+;                         :is_admin (sd/is-admin user_id (:tx request))
+;                         :authentication-method "Token"
+;                         )
+;
+;
+;          ;      :else (handler (assoc request
+;          ;                            :authenticated-entity entity
+;          ;                            :is_admin (sd/is-admin (or (:id entity) (:user_id entity)) tx)
+;          ;                            :authentication-method "Basic Authentication")))))
+;
+;          ]
+;
+;      (if [user-token (find-user-token-by-some-secret [token-secret] (:tx request))]
+;        ;(authenticate user-token is-admin? handler request)
+;        (do
+;          (println ">o> authenticate, param set!!")
+;          (println ">o> authenticate, param set!! user-token=" user-token)
+;          ;(authenticate user-token is-admin-admins? handler request))
+;          (authenticate user-token handler request))
+;        {:status 401
+;         :body {:message "No token for this token-secret found!"}})
+;
+;      )
+;
+;    ;(if-let [user-token (find-user-token-by-some-secret [token-secret] (:tx request))]
+;    ;  (authenticate user-token handler request)
+;    ;  {:status 401
+;    ;   :body {:message "No token for this token-secret found!"}})
+;
+;    (handler request)                                       ;; not auth
+;
+;    ))
+
+
 (defn find-and-authenticate-token-secret-or-continue [handler request]
-
-
-  (println ">o> abc1")
   (if-let [token-secret (find-token-secret-in-header request)]
-
-    (let [
-          _ (println ">o> abc1, TS=" token-secret)
-          user-token (find-user-token-by-some-secret [token-secret] (:tx request))
-          p (println ">o> user-token=" user-token)
-
-
-          user_id (:user_id user-token)
-          email (:email_address user-token)
-
-          ;(authenticate user-token handler request))
-
-          entity (get-user-by-login-or-email-address email (:tx request))
-
-          p (println ">o> user_id=" user_id)
-          p (println ">o> email=" email)
-
-          is-admin-auth-sys-user? (get-auth-systems-user user_id (:tx request))
-          p (println ">o> 1 is-admin-auth-sys-user?=" is-admin-auth-sys-user?)
-
-          is-admin-admins? (sd/is-admin user_id (:tx request)) ;;admins
-          p (println ">o> 2 is-admin-admins?=" is-admin-admins?)
-
-
-          request (assoc request
-                         :authenticated-entity entity
-                         :is_admin (sd/is-admin user_id (:tx request))
-                         :authentication-method "Token"
-                         )
-
-
-          ;      :else (handler (assoc request
-          ;                            :authenticated-entity entity
-          ;                            :is_admin (sd/is-admin (or (:id entity) (:user_id entity)) tx)
-          ;                            :authentication-method "Basic Authentication")))))
-
-          ]
-
-      (if [user-token (find-user-token-by-some-secret [token-secret] (:tx request))]
-        ;(authenticate user-token is-admin? handler request)
-        (do
-          (println ">o> authenticate, param set!!")
-          (println ">o> authenticate, param set!! user-token=" user-token)
-          (authenticate user-token is-admin-admins? handler request))
-        {:status 401
-         :body {:message "No token for this token-secret found!"}})
-
-      )
-
-    ;(if-let [user-token (find-user-token-by-some-secret [token-secret] (:tx request))]
-    ;  (authenticate user-token handler request)
-    ;  {:status 401
-    ;   :body {:message "No token for this token-secret found!"}})
-
-    (handler request)                                       ;; not auth
-
-    ))
+    (if-let [user-token (find-user-token-by-some-secret [token-secret] (:tx request))]
+      (authenticate user-token handler request)
+      {:status 401
+       :body {:message "No token for this token-secret found!"}})
+    (handler request)))
 
 (defn wrap [handler]
   (fn [request]
