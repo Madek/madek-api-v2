d8b1dd90cba5c53fb7187cce41a75212121f70cc
mradl <manuel.radl@zhdk.ch>
2024-08-06 12:12:29 +0200

    broken: id vs user_id

--- spec/authentication/public_user_admin_token_spec.rb
+++ spec/authentication/public_user_admin_token_spec.rb
@@ -6,6 +6,8 @@ shared_context :user_entity do |ctx|
   context "for Database User" do
     before :each do
       @entity = FactoryBot.create :user, password: "TOPSECRET"
+      @token = ApiToken.create user: @entity, scope_read: true,
+                               scope_write: true
     end
     let :entity_type do
       "User"
@@ -98,49 +100,54 @@ shared_context :test_proper_basic_auth do
 
     context "against GET endpoints " do
       {
-        "/api-v2/app-settings" => 200,
-        "/api-v2/context-keys" => 200,
-        "/api-v2/contexts" => 200,
-        "/api-v2/meta-keys" => 200,
-        "/api-v2/media-entry/5feea8b4-fb56-4002-a119-a66608266d89" => 404,
-        "/api-v2/media-entries?page=1&size=1" => 200,
-        "/api-v2/roles?page=1&size=1" => 200,
-        "/api-v2/vocabularies" => 200,
-        "/api-v2/custom_urls" => 200,
-        "/api-v2/full_texts?page=1&size=1" => 200,
-        "/api-v2/usage-terms" => 200,
-        "/api-v2/collections?page=1&size=1" => 200,
-        "/api-v2/keywords?page=1&size=2" => 200, # FIXME
-        "/api-v2/groups" => 200,
+        # "/api-v2/app-settings" => 200,
+        # "/api-v2/context-keys" => 200,
+        # "/api-v2/contexts" => 200,
+        # "/api-v2/meta-keys" => 200,
+        # "/api-v2/media-entry/5feea8b4-fb56-4002-a119-a66608266d89" => 404,
+        # "/api-v2/media-entries?page=1&size=1" => 200,
+        # "/api-v2/roles?page=1&size=1" => 200,
+        # "/api-v2/vocabularies" => 200,
+        # "/api-v2/custom_urls" => 200,
+        # "/api-v2/full_texts?page=1&size=1" => 200,
+        # "/api-v2/usage-terms" => 200,
+        # "/api-v2/collections?page=1&size=1" => 200,
+        # "/api-v2/keywords?page=1&size=2" => 200, # FIXME
+        # "/api-v2/groups" => 200,
+        #
+        # "/api-v2/auth-info" => 200,
 
-        "/api-v2/workflows" => 200,
-        "/api-v2/auth-info" => 200,
-        "/api-v2/edit_sessions?page=1&size=1" => 200,
+        "/api-v2/workflows" => 200, #  body={"message"=>"Not authorized. Please login."}
+        "/api-v2/edit_sessions?page=1&size=1" => 200 # body={"message"=>"Not authorized. Please login."},
 
-        "/api-v2/admin/admins" => 403,
-        "/api-v2/admin/app-settings" => 403,
-        "/api-v2/admin/context-keys" => 403,
-        "/api-v2/admin/contexts" => 403,
-        "/api-v2/admin/delegations" => 403,
-        "/api-v2/admin/delegation/users" => 403,
-        "/api-v2/admin/delegation/groups" => 403,
-        "/api-v2/admin/edit_sessions" => 403,
-        "/api-v2/admin/favorite/collections" => 403,
-        "/api-v2/admin/favorite/media-entries" => 403,
-        "/api-v2/admin/groups" => 403,
-        "/api-v2/admin/io_interfaces" => 403,
-        "/api-v2/admin/keywords" => 403,
-        "/api-v2/admin/meta-keys" => 403,
-        "/api-v2/admin/people" => 403,
-        "/api-v2/admin/roles?page=1&size=1" => 403,
-        "/api-v2/admin/usage-terms" => 403,
-        "/api-v2/admin/users" => 403,
-        "/api-v2/admin/static-pages" => 403,
-        "/api-v2/admin/vocabularies" => 403
+        # ,
+        #
+        # "/api-v2/admin/admins" => 403,
+        # "/api-v2/admin/app-settings" => 403,
+        # "/api-v2/admin/context-keys" => 403,
+        # "/api-v2/admin/contexts" => 403,
+        # "/api-v2/admin/delegations" => 403,
+        # "/api-v2/admin/delegation/users" => 403,
+        # "/api-v2/admin/delegation/groups" => 403,
+        # "/api-v2/admin/edit_sessions" => 403,
+        # "/api-v2/admin/favorite/collections" => 403,
+        # "/api-v2/admin/favorite/media-entries" => 403,
+        # "/api-v2/admin/groups" => 403,
+        # "/api-v2/admin/io_interfaces" => 403,
+        # "/api-v2/admin/keywords" => 403,
+        # "/api-v2/admin/meta-keys" => 403,
+        # "/api-v2/admin/people" => 403,
+        # "/api-v2/admin/roles?page=1&size=1" => 403,
+        # "/api-v2/admin/usage-terms" => 403,
+        # "/api-v2/admin/users" => 403,
+        # "/api-v2/admin/static-pages" => 403,
+        # "/api-v2/admin/vocabularies" => 403
 
       }.each do |url, code|
         it "accessing #{url}    results in expected status-code" do
-          response = basic_auth_plain_faraday_json_client(@entity.login, @entity.password).get(url)
+          # response = basic_auth_plain_faraday_json_client(@entity.login, @entity.password).get(url)
+          response = new_token_auth_faraday_json_client(@token.token, url)
+          binding.pry
           expect(response.status).to eq(code)
         end
       end
@@ -251,7 +258,8 @@ context "4) resource with token-user auth" do
   describe "Test status-code as madek-user " do
     it "against POST endpoints " do
       user_url = "/api-v2/admin/full_text/"
-      response = newbasic_auth_plain_faraday_json_client(token.token, nil).post(user_url) do |req|
+      # response = newbasic_auth_plain_faraday_json_client(token.token, nil).post(user_url) do |req|
+        response = new_token_auth_faraday_json_client(@token.token, "/api-v2/auth-info", :post) do |req|
         req.body = {
           text: "string",
           media_resource_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6"
@@ -305,7 +313,9 @@ context "4) resource with token-user auth" do
 
       }.each do |url, code|
         it "accessing #{url}    results in expected status-code" do
-          response = basic_auth_plain_faraday_json_client(token.token, nil).get(url)
+          # response = basic_auth_plain_faraday_json_client(token.token, nil).get(url)
+          response = new_token_auth_faraday_json_client(@token.token, "/api-v2/auth-info")
+
           expect(response.status).to eq(code)
         end
       end
