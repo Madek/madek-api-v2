474656a54ac42312d10df50092c5fb8cbb978aba
mradl <manuel.radl@zhdk.ch>
2024-08-06 10:07:12 +0200

    working example

--- src/madek/api/authentication/token.clj
+++ src/madek/api/authentication/token.clj
@@ -1,5 +1,6 @@
 (ns madek.api.authentication.token
   (:require
+   [clojure.string :as str]
    [clojure.walk :refer [keywordize-keys]]
    [honey.sql :refer [format] :rename {format sql-format}]
    [honey.sql.helpers :as sql]
@@ -33,7 +34,7 @@
            (sql/join :users [:= :users.id :api_tokens.user_id])
            (sql-format))
     (jdbc/execute! tx)
-    (map #(clojure.set/rename-keys % {:email :email_address}))
+    (map #(clojure.set/rename-keys % {:email :email_address :id :user_id}))
     first))
 
 (defn violates-not-read? [user-token request]
@@ -46,24 +47,27 @@
     (#{:delete :put :post :patch}
      (:request-method request))))
 
-;(defn authenticate [user-token is-admin? handler request]
-(defn authenticate [user-token handler request]
+(defn authenticate [user-token is-admin? handler request]
+;(defn authenticate [user-token handler request]
+  (println ">o> user-token???=" user-token)
+  (println ">o> is-admin????=" is-admin?)
+  (println ">o> uri????=" (:uri request))
   (cond
     (:token_revoked user-token) {:status 401
                                  :body "The token has been revoked."}
 
-    ;; TODO: reactivate it!!
-    ;(violates-not-read?
-    ;  user-token request) {:status 403
-    ;                       :body (str "The token is not allowed to read"
-    ;                                  " i.e. to use safe http verbs.")}
-    ;(violates-not-write?
-    ;  user-token request) {:status 403
-    ;                       :body (str "The token is not allowed to write"
-    ;                                  " i.e. to use unsafe http verbs.")}
+    ; TODO: reactivate it!!
+    (violates-not-read?
+      user-token request) {:status 403
+                           :body (str "The token is not allowed to read"
+                                      " i.e. to use safe http verbs.")}
+    (violates-not-write?
+      user-token request) {:status 403
+                           :body (str "The token is not allowed to write"
+                                      " i.e. to use unsafe http verbs.")}
 
-    ;(and (str/includes? (:uri request) "admin/") (not is-admin?) ) {:status 401
-    ;                             :body "The token has no admin-privileges."}
+    (and (str/includes? (:uri request) "/api-v2/admin/") (not is-admin?) ) {:status 401
+                                 :body "The token has no admin-privileges."}
 
     :else (do
             (println ">o> set admin now to _> " (sd/is-admin (:user_id user-token) (:tx request)))
@@ -85,6 +89,15 @@
                             (sql/where [:= :user_id userId] [:= :auth_system_id "password"])
                             sql-format)))
 
+
+(defn- get-user-by-login-or-email-address [login-or-email tx]
+  (->> (jdbc/execute! tx (-> (sql/select :*)
+                             (sql/from :users)
+                             (sql/where [:or [:= :login login-or-email] [:= :email login-or-email]])
+                             sql-format))
+    (map #(assoc % :type "User"))
+    (map #(clojure.set/rename-keys % {:email :email_address}))
+    first))
 (defn find-and-authenticate-token-secret-or-continue [handler request]
 
 
@@ -97,20 +110,28 @@
           p (println ">o> user-token=" user-token)
 
 
+          user_id (:user_id user-token)
+          email (:email_address user-token)
+
           ;(authenticate user-token handler request))
 
-          is-admin-auth-sys-user? (get-auth-systems-user (:user_id user-token) (:tx request))
-          p (println ">o> is-admin?=" is-admin-auth-sys-user?)
+          entity (get-user-by-login-or-email-address email (:tx request))
 
-          ;is-admin-admins? (sd/is-admin (or (:id entity) (:user_id entity)) tx)
-          ;p (println ">o> is-admin?=" is-admin-admins?)
+          p (println ">o> user_id=" user_id)
+          p (println ">o> email=" email)
+
+          is-admin-auth-sys-user? (get-auth-systems-user user_id (:tx request))
+          p (println ">o> 1 is-admin-auth-sys-user?=" is-admin-auth-sys-user?)
+
+          is-admin-admins? (sd/is-admin user_id (:tx request)) ;;admins
+          p (println ">o> 2 is-admin-admins?=" is-admin-admins?)
 
 
-          ;request (assoc request
-          ;                                           ;:authenticated-entity entity
-          ;                                           :is_admin (sd/is-admin (or (:id entity) (:user_id entity)) tx)
-          ;                                           ;:authentication-method "Basic Authentication"
-          ;               )
+          request (assoc request
+                         :authenticated-entity entity
+                         :is_admin (sd/is-admin user_id (:tx request))
+                         :authentication-method "Token"
+                         )
 
 
           ;      :else (handler (assoc request
@@ -124,7 +145,8 @@
         ;(authenticate user-token is-admin? handler request)
         (do
           (println ">o> authenticate, param set!!")
-          (authenticate user-token handler request))
+          (println ">o> authenticate, param set!! user-token=" user-token)
+          (authenticate user-token is-admin-admins? handler request))
         {:status 401
          :body {:message "No token for this token-secret found!"}})
 
@@ -135,7 +157,9 @@
     ;  {:status 401
     ;   :body {:message "No token for this token-secret found!"}})
 
-    (handler request)))
+    (handler request)                                       ;; not auth
+
+    ))
 
 (defn wrap [handler]
   (fn [request]
