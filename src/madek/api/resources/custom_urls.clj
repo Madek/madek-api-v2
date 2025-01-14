588aeda43e32a197ef7ebbfdc18815eca612fd13
mradl <manuel.radl@zhdk.ch>
2025-01-14 04:44:23 +0100

    wip v3

--- src/madek/api/resources/custom_urls.clj
+++ src/madek/api/resources/custom_urls.clj
@@ -162,13 +162,33 @@
 ; TODO custom urls response coercion
 (def query-routes
   ["/"
-   {:openapi {:tags ["api/custom_urls"]}}
+   {:openapi {:tags ["api/custom_urls *"]}}
    ["custom_urls"
     {:get {:summary (sd/sum_usr "Query and list custom_urls.")
            :handler handle_list-custom-urls
            :coercion reitit.coercion.schema/coercion
            :responses {200 {:description "Returns the custom_urls."
-                            :body s/Any}
+                            ;:body s/Any
+
+
+                            :body [{
+
+                                     :id s/Str
+                                     :media_entry_id (s/maybe s/Uuid)
+                                     :collection_id (s/maybe s/Uuid)
+
+                                     (s/optional-key :is_primary) s/Bool
+                                       (s/optional-key  :creator_id) s/Uuid
+                                         (s/optional-key  :updator_id) s/Uuid
+                                           (s/optional-key  :created_at) s/Any
+                                             (s/optional-key  :updated_at) s/Any
+
+
+                                    }]
+
+
+
+                            }
                        404 {:description "Not found."
                             :body s/Any}}
            :parameters {:query {(s/optional-key :full_data) s/Bool
